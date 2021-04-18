dd859f39ce2610324869e5b619fe40157c814539
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-04-17 22:29:56 -0400

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import Wrapper from './Wrapper/Wrapper';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import Wrapper from "./Wrapper/Wrapper";
 
 const App = () => (
-  <Wrapper />
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Wrapper} />
+      <Route path="*" component={Wrapper} />
+    </Switch>
+  </Router>
 );
 
 export default App;
