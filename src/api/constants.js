9b21dbb094b526fc6698fa9bc0c02aa61f2b1694
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-04-19 22:30:40 -0400

    changed routes

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,8 +1,7 @@
 export const apiConstants = {
-  BASE_URL: 'http://ec2-18-207-112-127.compute-1.amazonaws.com:8080',
-  STAR_RATING: '/rating',
-  REVIEW: '/rating/review',
-  TEST_HASH: '/test/patient',
-  // PHONE_NUMBER: '+11234567890',
-  PHONE_NUMBER: '+14372288183'
+  BASE_URL: "http://ec2-18-207-112-127.compute-1.amazonaws.com:8080",
+  STAR_RATING: "/rating",
+  REVIEW: "/rating/review",
+  TEST_HASH: "/test/patient",
+  PHONE_NUMBER: "+11234567890",
 };
