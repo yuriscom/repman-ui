709e8e4aefa23f3f5f218a042eaafeae1abded24
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-06-18 23:35:48 -0400

    fix: commented useless library

--- src/components/Pages/BadReviewPage.js
+++ src/components/Pages/BadReviewPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
 import {
   // RECEIVE_ALERT_TEXT,
   FEEDBACK_MIN_LENGTH,
--- src/components/Pages/GoodReviewPage.js
+++ src/components/Pages/GoodReviewPage.js
@@ -1,5 +1,5 @@
-import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import {
   ACKNOWLEDGE_RATING_API,
