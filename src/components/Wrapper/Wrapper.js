43780c767318466aa660a5ecdaa50eee0313ce39
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-09-10 16:52:07 -0400

    feat: removed trailing slash

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -53,7 +53,7 @@ const Wrapper = () => {
       location.pathname !== DEBUG_LINK &&
       clientWebsite
     ) {
-      const hash = location.pathname.slice(1);
+      const hash = location.pathname.slice(1, location.pathname.lastIndexOf('/'));
 
       if (hash !== "") {
         setHash(hash);
